4d02082a663d52efe232bbc153a6ece5a1620506
Niels Wesseling <Niels.wesseling@outlook.com>
2023-12-09 08:19:30 +0100

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# AutoJSDocComments README
-Small VScode extension that utilizes ChatGPT to add JSDoc comments to selected code.
+# AutoJSDocComments
+
+A small extension for VisualStudio Code that utilizes ChatGPT to write JSDoc comments.
+
+## Description
+We all know the hassle of writing comments even though we all know its value most of us find it a hassle to write them. 
+With this small extension you just highlight the function you want to have JS Doc comments for, press CTRL+SHIFT+P and execute `` Generate comment ``
+The extension will once ask you for an api key and will save it for future use.
+
+## ChatGPT api key
+For the extension to work you will need an api key from OpenAI. This can be generated at https://platform.openai.com/api-keys
--- extension.js
+++ extension.js
@@ -1,58 +1,133 @@
-const vscode = require('vscode');
 const axios = require('axios');
+const vscode = require('vscode');
+
+function stateManager(context) {
+    return {
+        read,
+        write,
+        clear,
+    };
+
+    function read() {
+        return {
+            apiKey: context.globalState.get('apiKey'),
+        };
+    }
+
+    async function write(newState) {
+        await context.globalState.update('apiKey', newState.apiKey);
+    }
+
+    async function clear() {
+        await context.globalState.update('apiKey', undefined);
+    }
+}
+
+async function handleApi(context) {
+    const state = stateManager(context);
+
+    const { apiKey } = state.read();
+
+    if (apiKey) {
+        return apiKey;
+    } else {
+        const inputApiKey = await vscode.window.showInputBox({
+            prompt: 'Please enter your ChatGPT API key',
+            password: true,
+        });
+
+        if (inputApiKey) {
+            await state.write({
+                apiKey: inputApiKey,
+            });
+            return inputApiKey;
+        } else {
+            throw new Error('API key is required for this operation.');
+        }
+    }
+}
 
 function activate(context) {
     let disposable = vscode.commands.registerCommand('autojsdoccomments.generateComment', async () => {
-        // const userQuery = await vscode.window.showInputBox({
-        //     placeHolder: 'Ask ChatGPT...',
-        // });
-		let highlighted;
-		let selectionRange;
+        let highlighted;
+        let selectionRange;
 
-		const editor = vscode.window.activeTextEditor;
-		const selection = editor.selection;
-		if (selection && !selection.isEmpty) {
-			selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-			highlighted = editor.document.getText(selectionRange);
-		}
+        const editor = vscode.window.activeTextEditor;
+        const selection = editor.selection;
+        if (selection && !selection.isEmpty) {
+            selectionRange = new vscode.Range(
+                selection.start.line,
+                selection.start.character,
+                selection.end.line,
+                selection.end.character
+            );
+            highlighted = editor.document.getText(selectionRange);
+        }
 
-		const userQuery = 'Generate JS Doc comment for this function: ' + highlighted;
+        const userQuery = 'Generate JSDoc comment for this function: ' + highlighted;
 
         if (userQuery) {
-            try {
-                const apiKey = ''; // Replace with your ChatGPT API key
-                const apiUrl = 'https://api.openai.com/v1/chat/completions'; // Adjust the API URL if needed
+            await vscode.window.withProgress({
+                location: vscode.ProgressLocation.Window,
+                title: "Communicating with ChatGPT...",
+                cancellable: false
+            }, async (progress) => {
+                try {
+                    const apiKey = await handleApi(context);
+                    const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
-                const response = await axios.post(apiUrl, {
-                    model: 'gpt-3.5-turbo',
-                    messages: [
-                        { role: 'system', content: 'You are a helpful assistant.' },
-                        { role: 'user', content: userQuery },
-                    ],
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${apiKey}`,
-                    },
-                });
+                    progress.report({ increment: 10, message: 'Fetching API key...' });
 
-                const chatGPTResponse = response.data.choices[0].message.content;
-                // vscode.window.showInformationMessage(`ChatGPT says: ${chatGPTResponse}`);
-				if (editor) {
-					editor.edit(editBuilder => {
-						editBuilder.insert(selectionRange.start, `${chatGPTResponse}\n`);
-					});
-				}
-            } catch (error) {
-                vscode.window.showErrorMessage('Error communicating with ChatGPT.');
-                console.error(error);
-            }
+                    const response = await axios.post(
+                        apiUrl,
+                        {
+                            model: 'gpt-3.5-turbo',
+                            messages: [
+                                { role: 'system', content: 'You are a helpful assistant.' },
+                                { role: 'user', content: userQuery },
+                            ],
+                        },
+                        {
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': `Bearer ${apiKey}`,
+                            },
+                        }
+                    );
+
+                    progress.report({ increment: 70, message: 'Communication successful...' });
+
+                    const chatGPTResponse = response.data.choices[0].message.content;
+
+                    if (editor) {
+                        editor.edit((editBuilder) => {
+                            editBuilder.insert(selectionRange.start, `${chatGPTResponse}\n`);
+                        });
+                    }
+
+                    progress.report({ increment: 100, message: 'Operation complete.' });
+                } catch (error) {
+                    vscode.window.showErrorMessage('Error communicating with ChatGPT. \n' + error.message + " \n\n FULL ERROR: " + JSON.stringify(error, undefined, 2));
+                }
+            });
+        }
+    });
+
+    let deleteApiKeyDisposable = vscode.commands.registerCommand('autojsdoccomments.deleteApiKey', async () => {
+        try {
+            const state = stateManager(context);
+            await state.clear();
+            vscode.window.showInformationMessage('API key deleted successfully.');
+        } catch (error) {
+            vscode.window.showErrorMessage('Error deleting API key.');
+            console.error(error);
         }
     });
 
     context.subscriptions.push(disposable);
+    context.subscriptions.push(deleteApiKeyDisposable);
 }
 
 module.exports = {
-    activate
+    activate,
 };
